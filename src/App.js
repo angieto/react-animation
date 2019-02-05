8a070851914699b2c6c9dcfcf3d4e5cde8ec1696
Angie To <angie.to@berkeley.edu>
2019-02-04 23:31:05 -0800

    styled grid and grid items, dynamically render data from json file

--- src/App.js
+++ src/App.js
@@ -5,16 +5,22 @@ import Grid from './Grid/Grid';
 import Rocket from './Rocket/Rocket';
 import Smoke from './Rocket/Smoke';
 
+import facts from './NASA_facts.json';
+
 //styles
 import styles from './App.module.css';
 
 class App extends Component {
+  state = {
+    facts
+  }
+
   render() {
     return (
       <div className={styles.app}>
         <Title />
         <Background />
-        <Grid />
+        <Grid facts={this.state.facts} />
         <Rocket />
         <Smoke />
       </div>
--- src/Grid/Grid.js
+++ src/Grid/Grid.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import GridItem from './GridItem';
+
+import styles from './Grid.module.css';
 
 export default class Grid extends React.Component {
+  renderFacts = () => {
+    return this.props.facts.map(item => 
+      <GridItem className={styles.grid}
+        key={item.id}
+        fact={item}
+      />
+    );
+  };
+
   render() {
     return (
-      <div>
-        <h1>Grid</h1>
-      </div>
+      <section className={styles.grid}>
+        { this.renderFacts() }  
+      </section>
     );
   }
 }
